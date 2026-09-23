15a43ca9ef12e67cffb665d63a1ae37fbe0af423
Gabriel <gabrielpariapaza773@gmail.com>
2026-09-22 21:58:02 -0500

    Agregar funcione de validación

--- GuiaDeLab_T2/Program.cs
+++ GuiaDeLab_T2/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static bool ValidarCodigo(string codigo)
+{
+    if (string.IsNullOrWhiteSpace(codigo))
+    {
+        return false;
+    }
+    if (codigo.Length < 6)
+    {
+        return false;
+    }
+    foreach (char caracter in codigo)
+    {
+        if (!char.IsDigit(caracter))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+// Valida que un texto obligatorio no esté vacío
+// ni contenga únicamente espacios.
+static bool ValidarTexto(string texto)
+{
+    return !string.IsNullOrWhiteSpace(texto);
+}
+// Valida que el tipo de consulta pertenezca
+// a las categorías permitidas.
+static bool ValidarTipoConsulta(string tipo)
+{
+    tipo = tipo.ToLower().Trim();
+    if (tipo == "matrícula" ||
+        tipo == "pagos" ||
+        tipo == "constancia" ||
+        tipo == "plataforma" ||
+        tipo == "otro")
+    {
+        return true;
+    }
+    return false;
+}
+}
